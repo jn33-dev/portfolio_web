4ce8f73deba86f4ca587237edb4d1a03a28cdbed
Yena_choi <geniusyena332@gmail.com>
2022-05-27 23:59:25 +0900

    JS modify navbar toggleing in media-quarry

--- main.js
+++ main.js
@@ -18,12 +18,12 @@ document.addEventListener('scroll', () => {
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-    
     const target = event.target;
     const link = target.dataset.link;
     if (link == null) {
         return;
     }
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 });
 
