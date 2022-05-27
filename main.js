5a308f5c66b11b82563de7bf282e8bf8cc8b9616
Yena_choi <geniusyena332@gmail.com>
2022-05-27 23:49:06 +0900

    JS navbar menu toggle & project UI twick

--- main.js
+++ main.js
@@ -13,6 +13,8 @@ document.addEventListener('scroll', () => {
     }
 });
 
+
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -23,6 +25,12 @@ navbarMenu.addEventListener('click', (event) => {
         return;
     }
     scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
 });
 
 // Handle scrolling when tapping on the contact me button
@@ -88,7 +96,6 @@ workBtnCotainer.addEventListener('click', (e) => {
 
     
 });
-
 
 
 
